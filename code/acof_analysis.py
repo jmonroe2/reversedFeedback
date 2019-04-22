b7e29c2983337c80433ecc431ff07b7ce28b43f1
John <jonathan0monroe@gmail.com>
2019-04-21 23:02:11 -0500

    Finish debugging AoT for forward, zi=0

--- code/acof_analysis.py
+++ code/acof_analysis.py
@@ -148,70 +148,59 @@ def calculate_AoT(weak_measurement, z0=0):
     sig2 = dV**2/S ## sigma squared
 
     ## forward probability
-    # updated 04/04/19
-    #gnd_gauss = np.exp( -(weak_measurement - ground_val)**2 *S/2/dV )
-    #ex_gauss = np.exp( -(weak_measurement - excited_val)**2 *S/2/dV )
     gnd_gauss = np.exp( -(weak_measurement - ground_val)**2 /2/sig2)
     ex_gauss = np.exp( -(weak_measurement - excited_val)**2 /2/sig2)
-    
     for_log_prob = np.log( (1+z0)/2 *gnd_gauss  +  (1-z0)/2*ex_gauss )
 
     ## backwards probability
-    #gnd_gauss2 = np.exp( -(-weak_measurement - ground_val)**2 *S/2/dV )
-    #ex_gauss2 = np.exp( -(-weak_measurement - excited_val)**2 *S/2/dV )
+    ## passive transformation: use negative record
     gnd_gauss_back = np.exp( -(-weak_measurement - ground_val)**2 /2/sig2)
     ex_gauss_back = np.exp( -(-weak_measurement - excited_val)**2 /2/sig2)
     
-    x_final, z_final = util.theory_xz(weak_measurement)
+    #x_final, z_final = util.theory_xz(weak_measurement)
     gamma = weak_measurement/sig2*ground_val
+    #z_final = np.tanh(gamma)
+    z_final = ( np.sinh(gamma) + z0*np.cosh(gamma) ) /( np.cosh(gamma) + z0*np.sinh(gamma) )
     #z_final = np.tanh(weak_measurement/sig2*ground_val) ##@ use above line instead; note scaling
-    z_final = np.tanh(gamma)
-    # active transformation: flip coordiate of z ##@ I think this is untrue
     back_log_prob = np.log( (1+z_final)/2 *gnd_gauss_back +  (1-z_final)/2*ex_gauss_back)
 
     ## log ratio
     Q = for_log_prob - back_log_prob
-    ##@ rename these: they should be in "units" 
-    hist_counts, hist_bins= np.histogram(Q, bins=num_bins)
-    
-    
+    Q_prob, bins = np.histogram(Q, bins=num_bins)
+    Q_prob_xs = bins[1:] ## use right-hand side of bin as x position
+
+    ## compare to theory
+    Q_analytic_prob  = get_analytic_probQ(Q_prob_xs, z0)
+    Q_analytic_prob *= num_measurements # scale prob. to occurances
+    #Q_analytic_prob *= 0.25 ##aribtrary to get "better looking" agreement
+ 
+    fig, q_hist_ax = plt.subplots() 
+    q_hist_ax.semilogy( Q_prob_xs, Q_prob,'k-', label="Hist counts")
+    q_hist_ax.semilogy( Q_prob_xs, Q_analytic_prob,'r--', label="Analytic")
+    q_hist_ax.set_xlabel("Q", fontsize=20)
+    q_hist_ax.set_ylabel("Counts", fontsize=20)
+    q_hist_ax.set_xlim(-2,2)
+    plt.legend()
+  
+    ## DEBUGGING 
+    ################################################ 
+    # compare analytic Q to exp. Q
+    '''
     ## compare to analytic results
     weak_sample = weak_measurement[:1500]
     gamma_sample = gamma[:1500]
     z_sample = z_final[:1500]
     pf_sample = np.exp(for_log_prob[:1500])
+    pf_sample /= np.sqrt(2*np.pi*sig2)
     Q_sample = Q[:1500]
- 
-    Q_analytic = get_analytic_q(weak_sample*ground_val,z0=0) ##@ note the scaling
-    Q_analytic_prob  = get_analytic_probQ(hist_bins[1:],z0=0)
-    #Q_analytic_prob *= num_measurements # scale prob. to occurances
-   
-    ################################################ 
-    ## plots
-    # compare analytic Q to exp. Q
-    #'''
-    fig, tmp_cf = plt.subplots()
-    tmp_cf.plot(Q_sample, pf_sample, '.k', label="data")
-    tmp_cf.plot(hist_bins[1:], Q_analytic_prob, label='thy')
-    #tmp_cf.plot(Q_sample,z_sample,'.k',label="data")
-    #tmp_cf.plot(hist_bins[1:], Q_analytic_prob, 'r.',label="thy")
-    tmp_cf.set_xlabel("Q")
-    tmp_cf.set_ylabel("Z")
-    plt.legend()
-    return 0;
-    #'''
 
-    fig, q_hist_ax = plt.subplots() 
-    q_hist_ax.semilogy( hist_bins[1:], hist_counts,'k-', label="Hist counts")
-    q_hist_ax.semilogy( hist_bins[1:], Q_analytic_prob,'r--', label="Analytic")
-    #q_hist_ax.plot( hist_bins[1:], Q_analytic_prob, 'r--')
-    #q_hist_ax.plot( Q_sample, tmp_sample, ',k')
-    #q_hist_ax.fill_between( hist_bins[1:], hist_counts,color='r', alpha=0.3)
-    q_hist_ax.set_xlabel("Q", fontsize=20)
-    q_hist_ax.set_ylabel("Counts", fontsize=20)
-    q_hist_ax.set_xlim(-2,2)
+    fig, tmp_cf = plt.subplots()
+    tmp_cf.plot(Q_sample, z_sample, '.k', label="data")
+    tmp_cf.plot(Q_prob_xs, Q_analytic_prob, label='thy')
+    tmp_cf.set_xlabel("Q")
+    tmp_cf.set_ylabel("$P_f(\gamma$|Q)")
     plt.legend()
-    plt.show()
+    #'''
 ##END calculate_AoT
     
 
@@ -239,20 +228,16 @@ def get_analytic_probQ(Q, z0=0):
     #dV = 3.31 
     S = 0.357 # from calc_
     dV = 2.5
-    tT = dV**2/S ## tau over T via equating Gaussian variance
+    tT = dV**2/S ## tau over T; equal to Gaussian variance
 
     gamma_Q = np.arccosh(np.exp(Q/2))
+    gamma_Q /= 1.4 # scale for ground state values
     # below not used but included for completeness
-    weak = gamma_Q*tT/1.4 # scale for ground state values
+    weak = gamma_Q*tT/1.4 # scale for dV
 
-    #zf_Q = np.sqrt(np.exp(Q)-1)/np.exp(Q) # includes cosh(gamma) factored from Gaussians
-    #zf_Q = np.sqrt(np.exp(Q)-1)/np.exp(Q/2) # not sure where this comes from...
-    ## additions: 04/05/19
-    zf_Q = np.tanh(gamma_Q) # Z(Q) = Z(g(Q)) = Z(g), g=gamma
-    ## end additions
-    arg = -0.5/tT -0.5*tT*gamma_Q**2
-    pf_Q = np.sqrt(tT/2/np.pi)* np.exp(arg)
-    return pf_Q
+    zf_Q = np.sqrt(np.exp(Q)-1)*np.exp(-Q/2.)
+    arg = -0.5/tT -0.5*tT*gamma_Q**2 +Q/2.
+    pf_Q = np.sqrt(1/2/np.pi/tT)*0.5 *np.exp(arg)*2
    
     return pf_Q/2/zf_Q
 ##END get_analytic_q
